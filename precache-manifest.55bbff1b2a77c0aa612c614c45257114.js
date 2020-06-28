8c48d3407e26c7770fed8e9c6aa7354a66bfde2f
Axellben <cretu.alexandru2000@gmail.com>
2020-06-28 18:01:37 +0300

    Updates

---
+++ precache-manifest.55bbff1b2a77c0aa612c614c45257114.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1037ef5e7884fc6b21f7b829e66bd649",
+    "url": "/TodoApp/index.html"
+  },
+  {
+    "revision": "f53c62f42281f7b66390",
+    "url": "/TodoApp/static/css/main.2f865dc1.chunk.css"
+  },
+  {
+    "revision": "652baa3519d36f60ab89",
+    "url": "/TodoApp/static/js/2.4648c0a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TodoApp/static/js/2.4648c0a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f53c62f42281f7b66390",
+    "url": "/TodoApp/static/js/main.95a5e6ea.chunk.js"
+  },
+  {
+    "revision": "4d769cb17efc837f2f59",
+    "url": "/TodoApp/static/js/runtime-main.689fce80.js"
+  }
+]);
